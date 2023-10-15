abb1fe0f36d8d28c1958ff71299806971338bc09
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-10-15 13:20:42 +0530

    Add files via upload

---
+++ Selection_Sort.py
@@ -0,0 +1,15 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        # Find the minimum element in the remaining unsorted list
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        # Swap the found minimum element with the first element
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+# Example usage:
+my_list = [64, 25, 12, 22, 11]
+selection_sort(my_list)
+print("Sorted list:", my_list)
