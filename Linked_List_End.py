a7793786ec822c13feb4abe23955401c71dffc21
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-10-17 19:42:33 +0530

    Add files via upload

---
+++ Linked_List_End.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self , data = None , next = None):
+        self.data = data
+        self.next = next
+    
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+    
+    def insert_At_begining(self , data):
+        node = Node(data , self.head)
+        self.head = node
+    
+    def print(self):    
+        if self.head is None:
+            print("NULL")
+            return 
+        else:
+            itr = self.head
+            llstr = ''
+            while itr:
+                llstr += str(itr.data)
+                itr = itr.next
+            print(llstr)
+            
+    def insert_At_End(self , data):
+        if self.head is None:
+            self.head = Node(data , None)
+            return
+        
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+            
+        itr.next = Node(data , None)
+            
+            
+if __name__ == '__main__':
+    ll = Linkedlist()
+    ll.insert_At_begining(18)
+    ll.insert_At_begining(45)
+    ll.insert_At_End(60)
+    ll.print()
+                
+                
+                
+                
+            
+        
+        
+        
+    
