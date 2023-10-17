574428f43af0843df41c9362e571cfff39b58812
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-10-17 21:46:13 +0530

    Update Linked_List_End.py

--- Linked_List_End.py
+++ Linked_List_End.py
@@ -1,31 +1,32 @@
-class Node:
-    def __init__(self , data = None , next = None):
-        self.data = data
-        self.next = next
+class Node: #Class of Node
+    def __init__(self , data = None , next = None): # Constructor containing data and next pointer
+        self.data = data #data
+        self.next = next #pointer to next node
     
-class Linkedlist:
-    def __init__(self):
-        self.head = None
+class Linkedlist: #Class of linkedlist
+    def __init__(self): #Constructor
+        self.head = None #head pointer pointing to NULL
     
-    def insert_At_begining(self , data):
-        node = Node(data , self.head)
-        self.head = node
+    def insert_At_begining(self , data): #Function for insert at beginning
+        node = Node(data , self.head) # new node is before head node
+        self.head = node # linking the nodes
     
-    def print(self):    
-        if self.head is None:
+    def print(self):  # function for printing the linkedlist
+        if self.head is None: # if head pointer is pointing NULL
             print("NULL")
             return 
         else:
-            itr = self.head
-            llstr = ''
+            # Iteration
+            itr = self.head # itr pointing to head
+            llstr = '' 
             while itr:
                 llstr += str(itr.data)
                 itr = itr.next
             print(llstr)
             
-    def insert_At_End(self , data):
+    def insert_At_End(self , data): #function for insert at end
         if self.head is None:
-            self.head = Node(data , None)
+            self.head = Node(data , None) # head is linking to new node and new node is linked to NULL
             return
         
         itr = self.head
@@ -35,8 +36,9 @@ class Linkedlist:
         itr.next = Node(data , None)
             
             
-if __name__ == '__main__':
+if __name__ == '__main__': #main function
     ll = Linkedlist()
+    # Node insertion
     ll.insert_At_begining(18)
     ll.insert_At_begining(45)
     ll.insert_At_End(60)
