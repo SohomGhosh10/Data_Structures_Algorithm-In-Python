fe92381e076a6edb28f8075fa19bce6d1866e84a
Sohom Ghosh <136338690+SohomGhosh10@users.noreply.github.com>
2023-12-01 22:38:43 +0530

    Update Circular_Linkedlist.py

--- Circular_Linkedlist.py
+++ Circular_Linkedlist.py
@@ -1,21 +1,21 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+    def __init__(self, data): # Constructor
+        self.data = data # data declaration
+        self.next = None # Node declaration
 
 
 class CircularLinkedList:
-    def __init__(self):
+    def __init__(self): # constructor
         self.head = None
 
-    def is_empty(self):
+    def is_empty(self): # Queue empty condition
         return self.head is None
 
-    def append(self, data):
+    def append(self, data): # Enqueue condition
         new_node = Node(data)
         if not self.head:
             self.head = new_node
-            new_node.next = self.head
+            new_node.next = self.head # Nodes linking
         else:
             temp = self.head
             while temp.next != self.head:
@@ -23,7 +23,7 @@ class CircularLinkedList:
             temp.next = new_node
             new_node.next = self.head
 
-    def display(self):
+    def display(self): # function to print
         if not self.head:
             print("List is empty")
             return
@@ -37,13 +37,14 @@ class CircularLinkedList:
 
 
 # Example usage:
-if __name__ == "__main__":
+if __name__ == "__main__": # main function
     ll = CircularLinkedList()
 
+    # Appending the datas in the queue
     ll.append(1)
     ll.append(2)
     ll.append(3)
     ll.append(4)
 
-    print("Circular Linked List:")
+    print("Circular Linked List:") # printing the datas
     ll.display()
